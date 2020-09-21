ca1a7de75280f9ae768db5d0b637056de0346d85
Ana Carolina Ferreira <souzacarol451@gmail.com>
2020-09-21 19:06:29 -0300

    Finalizando a estilização da tela de cadastro

---
+++ src/lib/utils.js
@@ -0,0 +1,22 @@
+module.exports = {
+    //FORMATANDO A DATA
+    date(timestamp){
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)//pegando os dois ultimos digitos
+
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return{
+            year,
+            month,
+            day,
+            iso:`${year}-${month}-${day}`,
+            birthDay:`${day}/${month}`,
+            format: `${day}/${month}/${year}`
+        }
+    },
+
+}
---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+const methodOverride = require('method-override')//habilitar o PUT e o DELETE no method
+
+//*************************************************************************/
+
+const server = express()//CRIAÇÃO DO SERVIDOR
+
+//*************************************************************************/
+
+//INTERCEPTADORES
+
+//Habilita o uso do req.body
+server.use(express.urlencoded({extended:true}))
+
+server.use(express.static('public'))
+
+//DEPENDENCIA QUE VAI SOBREESCREVER O MÉTODO
+server.use(methodOverride('_method'))//sobreescreve o metodo antes chama antes de chamar a rota
+
+server.use(routes)
+
+//configurando a template engine
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+//*************************************************************************/
+
+server.listen(5000, function () {
+    console.log('Server is running')
+}) 
